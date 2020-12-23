df42d19ab0598e318b0ee537064eba1cb9acf50b
Muste Abari <m.abari@hotmail.co.uk>
2020-12-23 10:32:48 +0000

    working on Jenkins

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@35.246.60.242:3306/projectdb"
-app.config["SECRET_KEY"] = 'privateproject'
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /opt/qa-project
+sudo python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 app.py
