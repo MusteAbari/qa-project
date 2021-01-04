3a76159ac2bf725dc722f69bb0cbea45e1a2ef93
Muste Abari <m.abari@hotmail.co.uk>
2021-01-04 10:51:20 +0000

    test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /opt/qa-project
+sudo python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install pytest pytest-cov
+
+pytest --cov=application --cov-report xml --cov-report
